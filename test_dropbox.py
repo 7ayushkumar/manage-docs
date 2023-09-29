b7ebbefcb2ce47928efb72e138c035713f3023a8
Ayush Kumar <ayush.kumar@msn.com>
2023-09-29 10:04:40 -0400

    test_dropbox.py

--- test_dropbox.py
+++ test_dropbox.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title='🦜🔗 Ask the Doc App')
 st.title('🦜🔗 Ask the Doc App')
 
 # Dropbox access token
-DROPBOX_ACCESS_TOKEN = 'sl.Bm_DBR-qeYYwwJ9OxkoEgDBbjzHbnMbnQYf4e_tWIe6adbMMTe-gkJNfoa2fJwsfgTB_5_ia64NsAxLgPJwd7LFi7WgwWRBI2tgbO8MtO6Mm_cPNmmjkow_6BuzxG68utXxpSmN_j08n1XAxPj0mjSc'  # Replace with your actual access token
+DROPBOX_ACCESS_TOKEN = 'sl.Bm8jxBX__OKwEG2QstJGBtVictWyU7KfbbnpdelGyPW0BGIS2CTV3avy_38DaFjuhkK3ZbuCwBRP0BBOwEwVnKPy0qkdmkt0iWCC6xPZY9QlmYaAJTYwsdhdja2aZsqy0JcBuYlRnTPoyhbt3Ag4vxc'  # Replace with your actual access token
 
 # Initialize Dropbox client
 dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
