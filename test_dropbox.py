f095f0842b6fe5214a472aeadb6bae1ef7da2100
Ayush Kumar <ayush.kumar@msn.com>
2023-10-01 00:49:42 -0400

    Update test_dropbox.py

--- test_dropbox.py
+++ test_dropbox.py
@@ -8,31 +8,21 @@ import dropbox
 st.set_page_config(page_title='🦜🔗 Ask the Doc App')
 st.title('🦜🔗 Ask the Doc App')
 
-# Dropbox access token
-DROPBOX_ACCESS_TOKEN = 'sl.Bm_gI7ZMtAGMyLffNW8VsfKJvzre6KvJc8MrYQM3HZjBkqOueJjEycfZwoPXpqBeE2yyee4tQYJ3-BnQ55Lx-Ozs4a2eNOY4M-ZEbg5P2AeGIMxCgbwdkiC_s70M0IXec1okEiYOdbFXU6xbxWyctLc'  # Replace with your actual access token
-
-# Initialize Dropbox client
-dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
-
 # Function to list files in the "test-llm" folder with extensions .pdf and .txt
-def list_dropbox_files(folder_path):
+def list_dropbox_files(folder_path, dbx):
     files = []
     try:
         for entry in dbx.files_list_folder(folder_path).entries:
             if isinstance(entry, dropbox.files.FileMetadata):
-                if entry.name.lower().endswith(('.pdf', '.txt','.docx')):
+                if entry.name.lower().endswith(('.pdf', '.txt','.docx','.xlsx','.csv')):
                     files.append(entry)
         return files
     except Exception as e:
         st.error(f"Failed to list files in Dropbox: {str(e)}")
         return []
 
-# List files in the "test-llm" folder
-dropbox_folder_path = '/test-llm'
-files_in_dropbox = list_dropbox_files(dropbox_folder_path)
-
 # Function to download files from Dropbox and store them as variables
-def download_and_store_files(files):
+def download_and_store_files(files, dbx):
     file_data = {}  # Dictionary to store file data
 
     for file_metadata in files:
@@ -47,7 +37,7 @@ def download_and_store_files(files):
     return file_data
 
 # Function to upload a file to Dropbox
-def upload_file_to_dropbox(file, folder_path):
+def upload_file_to_dropbox(file, folder_path, dbx):
     try:
         # Specify the path and file name in Dropbox
         dropbox_path = f"{folder_path}/{file.name}"
@@ -63,28 +53,34 @@ def upload_file_to_dropbox(file, folder_path):
         st.error(f"Failed to upload file '{file.name}' to Dropbox: {str(e)}")
         return False
 
-# Display the list of downloaded file names with delete options
-selected_files = st.multiselect("Select files to delete", [file.name for file in files_in_dropbox])
-if st.button("Delete Selected Files"):
-    for file_name in selected_files:
-        file_path = os.path.join(dropbox_folder_path, file_name)
-        try:
-            dbx.files_delete_v2(file_path)
-            st.success(f"File '{file_name}' deleted successfully.")
-        except Exception as e:
-            st.error(f"Failed to delete file '{file_name}' from Dropbox: {str(e)}")
+# Dropbox access token input field
+dropbox_access_token = st.text_input("Enter your Dropbox Access Token:")
+if dropbox_access_token:
+    # Initialize Dropbox client
+    dbx = dropbox.Dropbox(dropbox_access_token)
 
-# Display the list of downloaded file names
-st.write(f"Downloaded files from Dropbox:")
-for file_name in files_in_dropbox:
-    st.write(file_name.name)
+    # List files in the "test-llm" folder
+    dropbox_folder_path = '/test-llm'
+    files_in_dropbox = list_dropbox_files(dropbox_folder_path, dbx)
 
-# Now you can access the file contents using the downloaded_files dictionary
-# For example, to access the content of a file named 'example.pdf':
-# content = downloaded_files['example.pdf']
+    # Display the list of downloaded file names with delete options
+    selected_files = st.multiselect("Select files to delete", [file.name for file in files_in_dropbox])
+    if st.button("Delete Selected Files"):
+        for file_name in selected_files:
+            file_path = os.path.join(dropbox_folder_path, file_name)
+            try:
+                dbx.files_delete_v2(file_path)
+                st.success(f"File '{file_name}' deleted successfully.")
+            except Exception as e:
+                st.error(f"Failed to delete file '{file_name}' from Dropbox: {str(e)}")
 
-# Upload file button
-uploaded_file = st.file_uploader("Upload a new file to Dropbox", type=['pdf', 'txt','docx'])
-if uploaded_file is not None:
-    if upload_file_to_dropbox(uploaded_file, dropbox_folder_path):
-        st.success(f"File '{uploaded_file.name}' uploaded to Dropbox successfully.")
+    # Display the list of downloaded file names
+    st.write(f"Downloaded files from Dropbox:")
+    for file_name in files_in_dropbox:
+        st.write(file_name.name)
+
+    # Upload file button
+    uploaded_file = st.file_uploader("Upload a new file to Dropbox", type=['pdf', 'txt','docx','xlsx','csv'])
+    if uploaded_file is not None:
+        if upload_file_to_dropbox(uploaded_file, dropbox_folder_path, dbx):
+            st.success(f"File '{uploaded_file.name}' uploaded to Dropbox successfully.")
